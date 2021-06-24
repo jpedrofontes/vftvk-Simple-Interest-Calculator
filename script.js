c7f8abd4c29ac62c4de628f64fe869ba250c860d
João Pedro Fontes <mail@jpedrofontes.com>
2021-06-24 14:02:00 +0100

    Full functionality

--- script.js
+++ script.js
@@ -5,9 +5,8 @@ function compute()
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear() + parseInt(years);
-    var amount = principal;
 
-    document.getElementById("result").innerHTML = "If you deposit \<mark\>" + principal + "\</mark\>,\<br\>at an interest rate of \<mark\>" + rate + "%\</mark\>\<br\>You will receive an amount of \<mark\>" + amount + "\</mark\>,\<br\>in the year \<mark\>" + year + "\</mark\>\<br\>"    
+    document.getElementById("result").innerHTML = "If you deposit \<mark\>" + principal + "\</mark\>,\<br\>at an interest rate of \<mark\>" + rate + "%\</mark\>\<br\>You will receive an amount of \<mark\>" + interest + "\</mark\>,\<br\>in the year \<mark\>" + year + "\</mark\>\<br\>"    
 }
 
 function updateRate() 
